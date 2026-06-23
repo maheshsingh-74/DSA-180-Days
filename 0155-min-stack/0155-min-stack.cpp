fc42b280edb58a0a447d3af853b02e27c54129b0
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-06-23 21:47:10 +0530

    Time: 54 ms (31.86%), Space: 151.1 MB (33.48%) - LeetHub - Time: O(1), Space: O(n)

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,28 +1,51 @@
 class MinStack {
 public:
-stack<pair<int, int>>s;
-
+stack<long long int>st;
+long long int min_val;
     MinStack() {
         
     }
     
     void push(int val) {
-        if(s.empty()) s.push({val, val});
-        else
-        s.push({val, min(val,s.top().second)});
+    if (st.empty()) {
+            st.push(val);
+            min_val = val;
+        } else {
+            if (val >= min_val) {
+                st.push(val);
+            } else {
+                // Store 2*val - min_val to encode the previous min
+                st.push(2LL * val - min_val);
+                min_val = val;
+            }
+        }
     }
     
     void pop() {
-       s.pop(); 
+         if (st.empty()) return;
+        
+        long long t = st.top();
+        st.pop();
+
+        if (t < min_val) {
+            min_val = 2LL * min_val - t;
+        }
     }
     
     int top() {
-        return s.top().first;
+       if (st.empty()) return -1;
+        
+        long long t = st.top();
+        
+        if (t < min_val) {
+            return (int)min_val;
+        }
+        return (int)t;
         
     }
     
     int getMin() {
-      return s.top().second;  
+      return (int)min_val; 
     }
 };
 
