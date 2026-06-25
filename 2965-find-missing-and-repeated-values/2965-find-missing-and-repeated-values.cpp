a0912dbbc16d2ccafbcf0e6a6123d7bf0bd73586
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-06-25 10:29:23 +0530

    Time: 0 ms (100%), Space: 25.5 MB (90.56%) - LeetHub - Time: O(n^2), Space: O(1)

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int M=n*n;
-        unordered_map<int,int>mp;
-        int a=-1;
-        int b=-1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-              mp[grid[i][j]]++;
-
+        int n = grid.size();
+        long long M = n * n;
+        long long sum = 0;
+        long long sqsum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+                sqsum += grid[i][j] * grid[i][j];
             }
-            }
-            for(int i=1;i<=M;i++){
-                if(mp.count(i)==0) b=i;
-                else if(mp[i]==2) a=i;
-                if(a!=-1 && b!=-1) break;
-            }
-
-        
-        return {a,b};
+        }
+        long long expsum = (M * (M + 1)) / 2;
+        long long expsqsum = (M * (M + 1) * (2*M + 1)) / 6;
+        long long sumdiff = sum - expsum;
+        long long sqsumdiff = sqsum - expsqsum;
+        int a = ((sqsumdiff / sumdiff) + sumdiff)/2;
+        int b = ((sqsumdiff / sumdiff) - sumdiff)/2;
+        return {a, b};
     }
 };
 
