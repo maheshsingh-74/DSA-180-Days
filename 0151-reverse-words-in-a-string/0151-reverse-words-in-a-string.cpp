611adab305420ae53bd29f7624060be7dc672145
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-06-29 03:28:19 +0530

    Time: 0 ms (100%), Space: 10.2 MB (63.23%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+       int n=s.size();
+       string ans="";
+       reverse(s.begin(),s.end()); 
+
+       for(int i=0;i<n;i++){
+      string word="";
+        while(i<n && s[i]!=' '){
+       word+=s[i];
+       i++;
+
+        }
+        reverse(word.begin(),word.end());
+        if(word.length()>0){
+        ans+=" "+word;
+       }
+       }
+     
+       return ans.substr(1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
