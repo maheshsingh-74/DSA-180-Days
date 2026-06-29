9aa25d896cc2d9410c29ea6d55492b287866632c
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-06-29 13:07:22 +0530

    Added solution - LeetHub

---
+++ 700332-remove-loop-in-linked-list-GFG/700332-remove-loop-in-linked-list.cpp
@@ -0,0 +1,39 @@
+/* Structure of linked list Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    void removeLoop(Node* head) {
+        // code here
+        Node* slow=head;
+        Node* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) break;
+        }
+        if(fast==nullptr || fast->next==nullptr) return;
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        while(slow->next!=fast){
+            slow=slow->next;
+        }
+        slow->next=nullptr;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
