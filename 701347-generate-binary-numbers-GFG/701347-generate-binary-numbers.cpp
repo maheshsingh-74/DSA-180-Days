3e1a7d75a5730781842f36a395342eb0a0b5b73d
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-02 15:24:01 +0530

    Added solution - LeetHub

---
+++ 701347-generate-binary-numbers-GFG/701347-generate-binary-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<string> generateBinary(int n) {
+        vector<string> result;
+        queue<string> q;
+        
+        q.push("1");
+     
+        while (n--) {
+            string current = q.front();
+            q.pop();
+            result.push_back(current);
+            
+            q.push(current + "0");
+            q.push(current + "1");
+        }
+        
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
