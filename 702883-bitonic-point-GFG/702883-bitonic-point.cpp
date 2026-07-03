e10dfb01d3d586dd3b037f50ca6348a923c5b78a
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-04 03:33:41 +0530

    Added solution - LeetHub

--- 702883-bitonic-point-GFG/702883-bitonic-point.cpp
+++ 702883-bitonic-point-GFG/702883-bitonic-point.cpp
@@ -5,14 +5,14 @@ class Solution {
         int i=0;
         int j=arr.size()-1;
         int result;
-        while(i<j){
+        while(i<=j){
             int mid = i+(j-i)/2;
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 result=arr[mid];
                 break;
             }
             else if (arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) i=mid+1;
-            else j=mid;
+            else j=mid-1;
         }
         return result;
     }
