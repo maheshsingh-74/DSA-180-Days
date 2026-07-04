11e39a7c9e4be3dc0d27ec1f17c75698d6def0be
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-04 14:03:49 +0530

    Added solution - LeetHub

--- 702905-sort-a-string-GFG/702905-sort-a-string.cpp
+++ 702905-sort-a-string-GFG/702905-sort-a-string.cpp
@@ -1,9 +1,18 @@
 class Solution {
-  public:
+public:
     string sortString(string &s) {
-        // code here
-        sort(s.begin(),s.end());
-        return s;
+        vector<int> freq(26, 0);
+        
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+    
+        string sorted_s = "";
+        for (int i = 0; i < 26; i++) {
+            sorted_s.append(freq[i], 'a' + i);
+        }
+        
+        return sorted_s;
     }
 };
 
