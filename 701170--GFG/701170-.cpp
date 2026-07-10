3e462bb9e4050df9e5da84a3e181a51185307a0b
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-10 17:04:49 +0530

    Added solution - LeetHub

--- 701170--GFG/701170-.cpp
+++ 701170--GFG/701170-.cpp
@@ -1,11 +1,19 @@
 class Solution {
-  public:
-    bool isPowerofTwo(int n) {
-        // code here
-                return n>0 && ((n & (n-1)) == 0);
-
-    }
+	public:
+	bool isPowerofTwo(int n) {
+		// code here
+		// return n>0 && ((n & (n-1)) == 0);
+		if (n == 0)
+			return false;
+		while (n>1) {
+			if (n%2 != 0)
+				return false;
+			n /= 2;
+		}
+		return true;
+	}
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
