98bf9f13f8236085758516a2a618f3f0d6e00e76
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-15 00:10:03 +0530

    Time: 28 ms (38%), Space: 139.1 MB (68.36%) - LeetHub - Time: O(n), Space: O(k)

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n= nums.size();
+        deque<int>deq;
+        vector<int>result;
+        for(int i=0;i<n;i++){
+            while(!deq.empty() && deq.front()<=(i-k)){
+                deq.pop_front();
+            }
+            while(!deq.empty() && nums[i]>=nums[deq.back()]){
+                deq.pop_back();
+            }
+            deq.push_back(i);
+            if(i-k+1>=0){
+                result.push_back(nums[deq.front()]);
+            }
+
+        }
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
