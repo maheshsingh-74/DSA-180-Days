2b1d25c21844131690480e5b9464eb364574c92d
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-15 19:03:10 +0530

    Added solution - Time: O(N), Space: O(H)

---
+++ 700164-height-of-binary-tree-GFG/700164-height-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/*
+Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int height(Node* root) {
+        // code here
+          if(root==nullptr) return -1;
+        return 1+ max(height(root->left),height(root->right));
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
