3263d835a8f09ff045d1d4e09c62e5b1681c7791
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-22 13:23:32 +0530

    Added solution - Time: O(n), Space: O(n)

--- 700246-remove-duplicates-sorted-array-GFG/700246-remove-duplicates-sorted-array.cpp
+++ 700246-remove-duplicates-sorted-array-GFG/700246-remove-duplicates-sorted-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
-  public:
+public:
     vector<int> removeDuplicates(vector<int> &nums) {
-        // code here
-           int n=nums.size();
-        int i=0;
-        int j=1;
-        while(j<n){
-            if(nums[i]!=nums[j]){
-            i++;
-            nums[i]=nums[j];
+        if (nums.empty()) return {};
+        
+        vector<int> result;
+        result.push_back(nums[0]);
+        
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != result.back()) {
+                result.push_back(nums[j]);
             }
-            else j++;
         }
-        nums.resize(i+1);
-        return nums;
+        
+        return result;
     }
 };
 
