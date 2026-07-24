6f76c148ad98172d668b18b66ba20b8bf2374c77
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-24 13:49:24 +0530

    Added solution - Time: O(sqrt(n)), Space: O(log n)

---
+++ 899246-all-prime-factors-in-any-order-GFG/899246-all-prime-factors-in-any-order.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> primeFactors(int n) {
+        // code here
+        vector<int>ans;
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                ans.push_back(i);
+                n /= i;
+            }
+        }
+        
+        if (n > 1) {
+            ans.push_back(n);
+        }
+        
+        return ans;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
