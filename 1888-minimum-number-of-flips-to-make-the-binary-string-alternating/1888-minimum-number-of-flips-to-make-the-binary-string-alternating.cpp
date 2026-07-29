3b493e235e7630254c80a3da07bc71ee15345a8d
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-29 13:39:22 +0530

    Time: 54 ms (8.32%), Space: 28.5 MB (20.37%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        	int n= s.length();
+            s=s+s;
+            string s1, s2;
+            for(int i=0;i<2*n;i++){
+                s1+=(i%2) ? '0':'1';
+                 s2+=(i%2) ? '1':'0';
+            }
+            int flip1=0;int flip2=0;
+            int i=0,j=0;
+            int result=INT_MAX;
+            while(j<2*n){
+                if(s[j]!=s1[j])flip1++;
+                if(s[j]!=s2[j])flip2++;
+                if(j-i+1>n){
+                    if(s[i]!=s1[i])flip1--;
+                    if(s[i]!=s2[i])flip2--;
+                    i++;
+                }
+                if(j-i+1==n){
+                    result=min({flip1,flip2,result});
+                }
+                j++;
+            }
+		return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
