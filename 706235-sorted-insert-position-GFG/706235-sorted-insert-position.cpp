5a9336816efe5dc818aabc2b9f0f13913d0df318
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-30 23:47:35 +0530

    Force committed solution - Time: O(log n), Space: O(1)

---
+++ 706235-sorted-insert-position-GFG/706235-sorted-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &nums, int target) {
+        // code here
+          int n= nums.size();
+        int i=0;
+        int j= n-1;
+        while(i<=j){
+            int mid= i+(j-i)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target)i=mid+1;
+            else j=mid-1;
+        }
+        return i;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
