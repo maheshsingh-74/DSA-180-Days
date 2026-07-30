cd604d4e69ec049e2505c2d36b969386ffaf69d0
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-30 20:55:29 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 878537-implement-lower-bound-GFG/878537-implement-lower-bound.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+        int n =arr.size();
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid= i+(j-i)/2;
+            if(arr[mid]>=target){
+                j=mid-1;
+            }
+            else i=mid+1;
+        }
+        return i;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
