dc6d40ea493e2853a0d6c581f6f7451e3e6bf27f
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-07-31 14:52:23 +0530

    Added solution

---
+++ 703839-check-perfect-square-GFG/703839-check-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool isPerfectSquare(int n) {
+        // code here
+        int x=sqrt(n);
+        return x*x==n;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
