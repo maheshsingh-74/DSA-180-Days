a09045cda3227359ff4f621f1e7de3dbef1b2a24
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-03 01:20:37 +0530

    Time: 3576 ms (5.01%), Space: 0B (100%) - LeetHub - Time: O(N + M), Space: O(N + M)

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select e1.name , e2.bonus from Employee e1 left join Bonus e2 on e1.empId 
+=e2.empId where e2.bonus is null or e2.bonus<1000;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
