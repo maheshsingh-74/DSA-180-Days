14922fa6add498567a8b3b0663fef4992a463aa4
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-02 21:06:05 +0530

    Time: 1458 ms (21.15%), Space: 0B (100%) - LeetHub - Time: O(N + M), Space: O(N)

---
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select e2.unique_id , e1.name from EmployeeUNI e2 right join Employees e1 on e1.id=e2.id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
