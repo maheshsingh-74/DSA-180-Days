7b4d217e354fc0836743785c5cef655a8bca6e86
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-02 15:14:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.34%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    vector<int>dp;
+    int f(vector<int>& nums,int i){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        else return dp[i]= max(nums[i]+f(nums,i+2),f(nums,i+1));
+    }
+public: 
+    int rob(vector<int>& nums) {
+        dp.assign(nums.size(),-1);
+        return f(nums,0);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
