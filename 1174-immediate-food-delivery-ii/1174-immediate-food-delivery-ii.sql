26662d40e6bc58bf68d96479ea9e04f3573b7709
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-03 20:14:45 +0530

    Time: 734 ms (29.46%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+    ROUND(
+        AVG(order_date = customer_pref_delivery_date) * 100, 
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
