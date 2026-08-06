c6b56e05b3eba4a5cc38b841b62792ca1fcda686
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-06 14:55:24 +0530

    Time: 0 ms (100%), Space: 25.3 MB (89.79%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: void mindist(TreeNode *root, int &prev, int &ans){
+    if(!root) return;
+    mindist(root->left,prev, ans);
+    if(prev!=INT_MIN) ans= abs(min(ans, root->val-prev));
+    prev=root->val;
+    mindist(root->right,prev, ans);
+}
+    int getMinimumDifference(TreeNode* root) {
+         int prev= INT_MIN;
+        int ans=INT_MAX;
+        mindist(root, prev, ans);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
