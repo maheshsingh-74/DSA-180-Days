ceb267bb92a6385032d16df87041cd75e63fc9f3
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-06 13:51:51 +0530

    Time: 0 ms (100%), Space: 21.8 MB (74.85%) - LeetHub - Time: O(N), Space: O(N)

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
-public:void inorder(TreeNode *root, vector<int>&ans){
-      if(!root) return;
-      inorder(root->left, ans);
-      ans.push_back(root->val);
-      inorder(root->right, ans);
-  }
+public: bool bst(TreeNode *root,long long int &prev) {
+		if (!root)
+			return 1 ;
+		bool l =bst(root->left, prev);
+		if (l == 0)
+			return 0;
+		if (root->val <= prev)
+			return 0;
+		prev = root->val;
+	return	bst(root->right, prev);
+}
     bool isValidBST(TreeNode* root) {
-        vector<int>ans;
-        inorder(root,ans); 
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]<=ans[i-1])
-            return 0;
-        }
-        return 1;
+       long long int prev= LLONG_MIN;
+        return bst(root, prev);
     }
 };
 
