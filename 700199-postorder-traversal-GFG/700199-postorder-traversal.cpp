cb76451a73c975cd7f236b98dc980ee88b3870cc
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-11 23:14:51 +0530

    Added solution - Time: O(N), Space: O(N)

--- 700199-postorder-traversal-GFG/700199-postorder-traversal.cpp
+++ 700199-postorder-traversal-GFG/700199-postorder-traversal.cpp
@@ -1,4 +1,4 @@
-/*
+/* Structure of Binary Tree Node
 class Node {
   public:
     int data;
@@ -7,24 +7,23 @@ class Node {
 
     Node(int val) {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = nullptr;
     }
-};
-*/
-
+};*/
 class Solution {
-  public:
+private:
+    void solve(Node* root, vector<int>& result) {
+        if (root == nullptr) return;
+        
+        solve(root->left, result);       
+        solve(root->right, result);   
+        result.push_back(root->data);  
+    }
+
+public:
     vector<int> postOrder(Node* root) {
-        // code here
-         if(root==NULL) return {};
         vector<int> result;
-       // result.push_back(root->data); 
-        vector<int> left = postOrder(root->left); 
-        vector<int> right = postOrder(root->right); 
-        result.insert(result.end(), left.begin(), left.end());
-        result.insert(result.end(), right.begin(), right.end());
-          result.push_back(root->data); 
+        solve(root, result);
         return result;
     }
 };
