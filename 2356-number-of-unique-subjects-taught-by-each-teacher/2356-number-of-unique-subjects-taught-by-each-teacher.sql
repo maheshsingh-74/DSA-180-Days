ddeea8540ee281536afe0f86e05ba1672e402fe4
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-12 15:08:23 +0530

    Time: 674 ms (14.23%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select teacher_id,count(distinct subject_id) as cnt from Teacher 
+group by teacher_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
