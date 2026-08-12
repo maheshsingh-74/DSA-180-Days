c8c2ae3f7523e2f1f87782ba66474ab09a4a2ead
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-13 01:04:59 +0530

    Time: 609 ms (45.41%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,12 +1,13 @@
 SELECT employee_id, department_id
 FROM Employee
 WHERE primary_flag = 'Y'
-   OR employee_id IN (
-       SELECT employee_id
-       FROM Employee
-       GROUP BY employee_id
-       HAVING COUNT(department_id) = 1
-   );
+
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(department_id) = 1;
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
