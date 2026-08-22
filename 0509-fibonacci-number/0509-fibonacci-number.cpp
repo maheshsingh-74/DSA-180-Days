bd12f5ac2ef8e871b04a16c2f36746e42bbd87dc
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-22 17:47:37 +0530

    Time: 23 ms (22.5%), Space: 8.4 MB (11.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
-public:
+public: int find(int n,vector<int>&dp){
+    if(dp[n]!=-1) return dp[n];
+    else return find(n-1,dp)+find(n-2,dp);
+}
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-    return fib(n - 1) + fib(n - 2);
-      
+        if (n == 0) return 0;
+        
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return find(n,dp);
     }
 };
