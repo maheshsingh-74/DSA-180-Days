2d1a3290c6611fcda6f2169de1a3020e1fa54809
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-24 11:03:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.37%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0)
+            return 1;
+        int start = 9;
+        int current = 9;
+        int ans = 10;
+        while (n-- > 1 && start) {
+            current *= start--;
+            ans += current;
+        }
+        return ans;
+    }
+};
