fd4fb83da0d262a7cefa9560853a10e2b1d22278
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-26 11:49:46 +0530

    Time: 399 ms (23.8%), Space: 90.1 MB (5.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,17 @@
 class Solution {
-public: int solve(vector<int>& nums,int idx,int sum,int target){
+public: int solve(vector<int>& nums,int idx,int sum,int target, unordered_map<string,int>&mp){
     if(idx==nums.size()){
         if(sum==target)return 1;
         else return 0;
     }
-    int plus=solve(nums,idx+1,sum+nums[idx],target);
-    int pluss=solve(nums,idx+1,sum-nums[idx],target);
-    return plus+pluss;
+    string key=to_string(idx)+'_'+to_string(sum);
+    if(mp.count(key))return mp[key];
+    int plus=solve(nums,idx+1,sum+nums[idx],target,mp);
+    int pluss=solve(nums,idx+1,sum-nums[idx],target,mp);
+    return mp[key]= plus+pluss;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,0,0,target);
+        unordered_map<string,int>mp;
+        return solve(nums,0,0,target,mp);
     }
 };
