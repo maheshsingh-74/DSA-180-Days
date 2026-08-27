27aa2d89139a4e1f2fd4568cab02e18c4539047e
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-27 17:42:58 +0530

    Time: 21 ms (84.92%), Space: 12.7 MB (89.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,15 +1,22 @@
 class Solution {
     int t[1001][1001];
-public: int find(int n, int m,string &s1,string &s2){
+public: /*int find(int n, int m,string &s1,string &s2){
     if(n==0||m==0)return 0;
     if(t[n][m]!=-1)return t[n][m];
     if(s1[n-1]==s2[m-1]) return t[n][m]= 1+find(n-1,m-1,s1,s2);
     else return t[n][m]= max(find(n-1,m,s1,s2),find(n,m-1,s1,s2));
-}
+}*/
     int longestCommonSubsequence(string s1, string s2) {
         int n= s1.size();
         int m=s2.size();
-        memset(t,-1,sizeof(t));
-        return find(n,m,s1,s2);
+        memset(t,0,sizeof(t));
+        for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+       if(s1[i-1]==s2[j-1]) t[i][j]= 1+ t[i-1][j-1];
+       else t[i][j]= max(t[i-1][j],t[i][j-1]);
+
+        }}
+      //  return find(n,m,s1,s2);
+      return t[n][m];
     }
 };
