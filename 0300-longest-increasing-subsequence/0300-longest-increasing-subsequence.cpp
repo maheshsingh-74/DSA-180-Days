444923cb0d2fe06789e76b538059544ab67e77bb
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-08-29 15:40:19 +0530

    Time: 75 ms (49.97%), Space: 14.2 MB (71.19%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
-public:int t[2502][2502];
-    int solve(vector<int>& nums, int i, int p) {
-        if (i >= nums.size())
-            return 0;
-            if(p!=-1 && t[i][p]!=-1)return t[i][p];
-        int take = 0;
-        if (p == -1||nums[p]<nums[i]) take=1+solve(nums,i+1,i);
-        int skip=solve(nums,i+1,p);
-        if(p!=-1)
-        return t[i][p]= max(take,skip);
-        return max(take,skip);
-    }
+public:
     int lengthOfLIS(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-         return solve(nums, 0, -1);
-          }
+       int n= nums.size();
+       int maxLIS=1;
+       vector<int>t(n,1);
+       for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i]){
+                t[i]=max(t[i],t[j]+1);
+                maxLIS=max(maxLIS,t[i]);
+            }
+        }
+       } 
+       return maxLIS;
+    }
 };
