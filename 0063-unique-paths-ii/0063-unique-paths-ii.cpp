cd143fa287db0ee0852981e154db6ba3c3508b68
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-09-13 04:32:36 +0530

    Time: 0 ms (100%), Space: 11.4 MB (93.99%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,37 +1,20 @@
 class Solution {
-public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> t(m, vector<int>(n, 0));
-        for (int col = 0; col < n; col++) {
-            if (col >= 1 && grid[0][col - 1] == 1) {
-                grid[0][col] = 1;
-                t[0][col] = 0;
-            } else if (grid[0][col] == 1) {
-                t[0][col] = 0;
-            } else {
-                t[0][col] = 1;
-            }
-        }
-        for (int row = 0; row < m; row++) {
-            if (row > 0 && grid[row - 1][0] == 1) {
-                t[row][0] = 0;
-                grid[row][0] = 1;
-            } else if (grid[row][0] == 1)
-                t[row][0] = 0;
-            else
-                t[row][0] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    t[i][j] = 0;
-                } else {
-                    t[i][j] = t[i - 1][j] + t[i][j - 1];
-                }
-            }
-        }
-    return t[m-1][n-1];
+    int t[101][101];
+    int m,n;
+public: int solve(vector<vector<int>>& obstacleGrid,int i, int j){
+    if(i < 0 || i >= m || j < 0 || j >= n || obstacleGrid[i][j] != 0) 
+            return 0;
+            if(i==m-1 && j==n-1) return 1;
+            if(t[i][j]!=-1)return t[i][j];
+             int right = solve(obstacleGrid, i, j+1);
+        int down  = solve(obstacleGrid, i+1, j);
+          return t[i][j] = right + down;
+}
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        m=obstacleGrid.size();
+        n=obstacleGrid[0].size();
+       memset(t,-1,sizeof(t));
+       return solve(obstacleGrid,0,0);
+    
     }
 };
